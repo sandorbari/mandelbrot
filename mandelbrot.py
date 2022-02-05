935e2e17502beaa91707bb60b789d68ac1db19ee
Sandor Bari <bari.sandor@gmail.com>
2022-02-05 16:31:50 +0000

    First cut at generating mandelbrot set

--- mandelbrot.py
+++ mandelbrot.py
@@ -22,18 +22,46 @@ def escape_time(c: Complex, limit: int) -> int:
     for i in range(0, limit):
         if norm_square(z) > 4.0:
             return i
+        z = z * z + c
     
     return None
 
+
+def pixel_to_point(bounds: Tuple[int], pixel: Tuple[int], upper_left: complex, lower_right: complex) -> complex:
+    
+    width, height = lower_right.real - upper_left.real, upper_left.imag - lower_right.imag
+
+    real_part = upper_left.real + pixel[1] * width / bounds[1]
+    im_part = upper_left.imag - pixel[0] * height / bounds[0]
+
+    return complex(real_part, im_part)
+
+
 def render(pixels: npt.DTypeLike, bounds: Tuple[int], upper_left: complex, lower_right: complex):
-    pass
+    
+    assert(pixels.shape == bounds)
+
+    for row in range(0, bounds[0]):
+        for column in range(0, bounds[1]):
+            point = pixel_to_point(bounds, (row, column), upper_left, lower_right)
+            point_escape_time = escape_time(point, 255)
+
+            if point_escape_time == None:
+                pixels[row, column] = 0 # it's initialized to 0 anyway, just being explicit
+            else:
+                pixels[row, column] = 255 - point_escape_time
+
 
 def save_image(filename: str, pixels: npt.DTypeLike):
-    pass
+    image = Image.fromarray(pixels)
+    image = image.convert("L")
+    image.save(filename)
+
 
 # TODO - input validation
 def parse_complex(parts: List[str]) -> Complex:
     return complex(float(parts[0]), float(parts[1]))
+
 
 def main():
 
@@ -44,13 +72,13 @@ def main():
     
     filename: str = sys.argv[1]
     bounds: List[int] = [int(x) for x in sys.argv[2].split("x")]
-    columns, rows = bounds[0], bounds[1]
+    rows, columns = bounds[0], bounds[1]
     upper_left: complex = parse_complex(sys.argv[3].split(","))
     lower_right: complex = parse_complex(sys.argv[4].split(","))
 
-    pixels: npt.DTypeLike = np.zeros((columns, rows))
+    pixels: npt.DTypeLike = np.zeros((rows, columns), dtype=float) # TODO - move this within the render function
     
-    render(pixels, bounds, upper_left, lower_right)
+    render(pixels, (rows, columns), upper_left, lower_right)
     save_image(filename, pixels)
 
 
--- mandelbrot_tests.py
+++ mandelbrot_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from mandelbrot import parse_complex, norm_square
+from mandelbrot import parse_complex, norm_square, pixel_to_point
 from typing import List
 
 class TestComplexParser(unittest.TestCase):
@@ -22,7 +22,7 @@ class TestComplexParser(unittest.TestCase):
 class TestComplexArithmetics(unittest.TestCase):
 
     def test_calculate_square(self):
-        
+
         c: complex = complex(1, 2)
         expected_norm_square: float = 5.0
 
@@ -30,6 +30,19 @@ class TestComplexArithmetics(unittest.TestCase):
 
         self.assertEqual(expected_norm_square, calculated_norm_square)
 
+class TestPixelToPoint(unittest.TestCase):
+
+    def test_pixel_to_point(self):
+
+        bounds = (100, 200)
+        pixel = (25, 175)
+        upper_left = complex(-1.0, 1.0)
+        lower_right = complex(1.0, -1.0)
+        expected = complex(-0.5, -0.75)
+        
+        actual = pixel_to_point(bounds, pixel, upper_left, lower_right)
+
+        self.assertEqual(expected, actual)
 
 if __name__ == "__main__":
     unittest.main()
