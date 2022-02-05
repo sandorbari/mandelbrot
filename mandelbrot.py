f012c51bc33a954b9e8973768ef6d2bdf803d1cf
Sandor Bari <bari.sandor@gmail.com>
2022-02-05 13:36:12 +0000

    Calculating complex norm square; introducing numpy

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,14 +1,34 @@
-import cmath
+import numpy as np
+import numpy.typing as npt
 import sys
 
 from numbers import Complex
 from PIL import Image
-from typing import List
+from typing import List, Tuple
+# from __future__ import annotations for Python 3.9 <
 
-def escape_time(c: Complex):
+def norm_square(c: Complex) -> float:
+
+    '''
+    Calculates the normalized square of a complex number
+    '''
+
+    return c.real**2 + c.imag**2
+
+def escape_time(c: Complex, limit: int) -> int:
+    
+    z = complex(0, 0)
+
+    for i in range(0, limit):
+        if norm_square(z) > 4.0:
+            return i
+    
+    return None
+
+def render(pixels: npt.DTypeLike, bounds: Tuple[int], upper_left: complex, lower_right: complex):
     pass
 
-def save_image():
+def save_image(filename: str, pixels: npt.DTypeLike):
     pass
 
 # TODO - input validation
@@ -24,8 +44,15 @@ def main():
     
     filename: str = sys.argv[1]
     bounds: List[int] = [int(x) for x in sys.argv[2].split("x")]
-    upperleft: Complex = parse_complex(sys.argv[3].split(","))
-    lowerright = parse_complex(sys.argv[4].split(",")) # TODO - add type hint here as well; left out for demonstration purposes
+    columns, rows = bounds[0], bounds[1]
+    upper_left: complex = parse_complex(sys.argv[3].split(","))
+    lower_right: complex = parse_complex(sys.argv[4].split(","))
+
+    pixels: npt.DTypeLike = np.zeros((columns, rows))
+    
+    render(pixels, bounds, upper_left, lower_right)
+    save_image(filename, pixels)
+
 
 if __name__ == "__main__":
     main()
--- mandelbrot_tests.py
+++ mandelbrot_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from mandelbrot import parse_complex
+from mandelbrot import parse_complex, norm_square
 from typing import List
 
 class TestComplexParser(unittest.TestCase):
@@ -10,7 +10,7 @@ class TestComplexParser(unittest.TestCase):
         # Arrange
 
         complex_as_string_list: List[str]  = ["1", "2"]
-        complex_number = complex(1, 2)
+        complex_number: complex = complex(1, 2)
 
         # Act
 
@@ -18,6 +18,17 @@ class TestComplexParser(unittest.TestCase):
 
         # Assert
         self.assertEqual(complex_number, complex_parsed)
+
+class TestComplexArithmetics(unittest.TestCase):
+
+    def test_calculate_square(self):
+        
+        c: complex = complex(1, 2)
+        expected_norm_square: float = 5.0
+
+        calculated_norm_square: float = norm_square(c)
+
+        self.assertEqual(expected_norm_square, calculated_norm_square)
 
 
 if __name__ == "__main__":
